222a734db3a53355707d264e86cdafc1806d5cae
felixcatto <felix0808@yandex.ru>
2018-05-29 12:14:00 +0400

    make it work

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+import path from 'path';
+import webpack from 'webpack';
+
+
+const config = {
+  entry: {
+    index: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    filename: '[name].js',
+    publicPath: '/js',
+  },
+  devtool: 'cheap-module-eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['env', { modules: false }],
+              'stage-2',
+              'react',
+            ],
+          },
+        },
+      },
+    ],
+  },
+};
+
+export default config;
